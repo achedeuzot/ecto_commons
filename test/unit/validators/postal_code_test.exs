05218fa6135820155e2ac41faf2292c95e82d368
achedeuzot <klemen+git@sever.fr>
2021-08-12 22:43:45 +0200

    fix(postal_codes): Support european prefixes for some countries postal codes

--- test/unit/validators/postal_code_test.exs
+++ test/unit/validators/postal_code_test.exs
@@ -4,71 +4,89 @@ defmodule EctoCommons.PostalCodeValidatorTest do
   doctest EctoCommons.PostalCodeValidator, import: true
   alias EctoCommons.PostalCodeValidator
 
-  describe "French postal codes" do
-    @fr_valid_codes ["81100", "42380", "81570", "97110", "20090", "75001", "75 008"]
-    @fr_invalid_codes ["99130", "A8100", "42", "2A500"]
+  describe "postal codes" do
+    @valid_codes [
+      # France
+      {"fr", "81100"},
+      {"fr", "42380"},
+      {"fr", "81570"},
+      {"fr", "97110"},
+      {"fr", "20090"},
+      {"fr", "75001"},
+      {"fr", "75 008"},
 
-    for code <- @fr_valid_codes do
-      test "valid code: #{code}" do
-        types = %{postal_code: :string}
-        params = %{postal_code: unquote(code)}
+      # Great britain
+      {"gb", "W1J7NT"},
+      {"gb", "DE128HJ"},
+      {"gb", "HD75UZ"},
+      {"gb", "W21JB"},
+      {"gb", "SW1W 0NY"},
+      {"gb", "PO16 7GZ"},
+      {"gb", "GU16 7HF"},
+      {"gb", "L1 8JQ"},
 
-        result =
-          Ecto.Changeset.cast({%{}, types}, params, Map.keys(types))
-          |> PostalCodeValidator.validate_postal_code(:postal_code, country: "fr")
-
-        assert result.valid? == true
-      end
-    end
-
-    for code <- @fr_invalid_codes do
-      test "invalid code: #{code}" do
-        types = %{postal_code: :string}
-        params = %{postal_code: unquote(code)}
-
-        result =
-          Ecto.Changeset.cast({%{}, types}, params, Map.keys(types))
-          |> PostalCodeValidator.validate_postal_code(:postal_code, country: "fr")
-
-        assert result.valid? == false
-      end
-    end
-  end
-
-  describe "Great Britain postal_code" do
-    @gb_valid_codes [
-      "W1J7NT",
-      "DE128HJ",
-      "HD75UZ",
-      "W21JB",
-      "SW1W 0NY",
-      "PO16 7GZ",
-      "GU16 7HF",
-      "L1 8JQ"
+      # European crountries with prefix on postal codes
+      # See https://publications.europa.eu/code/en/en-390105.htm
+      # Luxemburg
+      {"fi", "FI-12345"},
+      {"fi", "AX-12345"},
+      {"hr", "HR-12345"},
+      {"lt", "LT-12345"},
+      {"lu", "1234"},
+      {"lu", "L-1234"},
+      {"lv", "LV-1234"},
+      {"se", "SE-12345"},
+      {"si", "SI-1234"}
     ]
-    @gb_invalid_codes ["A1380", "BC500"]
+    @invalid_codes [
+      # France
+      {"fr", "99130"},
+      {"fr", "A8100"},
+      {"fr", "42"},
+      {"fr", "2A500"},
 
-    for code <- @gb_valid_codes do
-      test "valid code: #{code}" do
+      # Great Britain
+      {"gb", "A1380"},
+      {"gb", "BC500"},
+
+      # Luxemburg
+      {"fi", "FI12345"},
+      {"fi", "F12345"},
+      {"fi", "AX12345"},
+      {"fi", "A12345"},
+      {"hr", "HR12345"},
+      {"hr", "H12345"},
+      {"lt", "LT12345"},
+      {"lt", "L12345"},
+      {"lu", "L1234"},
+      {"lv", "LV1234"},
+      {"se", "SE12345"},
+      {"se", "S12345"},
+      {"si", "SI1234"},
+      {"si", "S1234"}
+    ]
+
+    for {country, code} <- @valid_codes do
+      test "valid code for country #{country}: #{code}" do
         types = %{postal_code: :string}
         params = %{postal_code: unquote(code)}
 
         result =
           Ecto.Changeset.cast({%{}, types}, params, Map.keys(types))
-          |> PostalCodeValidator.validate_postal_code(:postal_code, country: "gb")
+          |> PostalCodeValidator.validate_postal_code(:postal_code, country: unquote(country))
 
         assert result.valid? == true
       end
     end
 
-    for code <- @gb_invalid_codes do
-      test "invalid code: #{code}" do
+    for {country, code} <- @invalid_codes do
+      test "invalid code for country #{country}: #{code}" do
         types = %{postal_code: :string}
         params = %{postal_code: unquote(code)}
 
         result =
           Ecto.Changeset.cast({%{}, types}, params, Map.keys(types))
-          |> PostalCodeValidator.validate_postal_code(:postal_code, country: "gb")
+          |> PostalCodeValidator.validate_postal_code(:postal_code, country: unquote(country))
 
         assert result.valid? == false
       end
