1b60497ac124b053f67b398f7478a1a998db0236
achedeuzot <klemen+git@sever.fr>
2024-04-03 13:20:00 +0200

    fix(postal_code): Fix postal code on unknown country code

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,7 @@
 [
   {":0:unknown_function Function :http_uri.parse/1 does not exist.", :unknown_function, 0},
+  {"lib/validators/email.ex", :unknown_function, 110},
+  {"lib/validators/email.ex", :unknown_function, 98},
+  {":0:unknown_function Function Burnex.check_domain_mx_record/1 does not exist.", :unknown_function, 0},
+  {":0:unknown_function Function Burnex.is_burner?/1 does not exist.", :unknown_function, 0},
 ]
